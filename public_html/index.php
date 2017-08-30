264c9d8ec8e5f3c6a5ff8a45485c65d91beb14a5
steve2482 <stevecb12684@yahoo.com>
2017-08-30 17:06:24 -0400

    initial commit

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Fleet Manager</title>
+    <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
+  </head>
+  <body>
+    <h1>This is the fleet manager</h1>
+  </body>
+</html>
